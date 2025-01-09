c9b3fdb5af386e661c72ec6d2a16deb4b8f067f1
doducthuan <doducthuan29@gmail.com>
2025-01-09 16:26:15 +0700

    learn create a component

---
+++ src/header-layout/header-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component, isStandalone } from "@angular/core";
+
+@Component({
+    selector: "header-layout",
+    standalone: true,
+    templateUrl: "./header-layout.component.html",
+    styleUrl: "./header-layout.component.scss"
+})
+
+export class HeaderComponent{}
+
